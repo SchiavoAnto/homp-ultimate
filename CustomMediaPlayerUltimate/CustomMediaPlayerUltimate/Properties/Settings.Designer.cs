6833c850be33f620796e379b995e95e35dd8314c
schiavoanto <scalarino04@gmail.com>
2024-01-31 14:53:03 +0100

    Added settings page
    - Also added the option to use search results as shuffle source instead of all songs.

--- CustomMediaPlayerUltimate/CustomMediaPlayerUltimate/Properties/Settings.Designer.cs
+++ CustomMediaPlayerUltimate/CustomMediaPlayerUltimate/Properties/Settings.Designer.cs
@@ -58,5 +58,17 @@ namespace CustomMediaPlayerUltimate.Properties {
                 this["PlayerShuffle"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool UseSearchResultsAsShuffleSource {
+            get {
+                return ((bool)(this["UseSearchResultsAsShuffleSource"]));
+            }
+            set {
+                this["UseSearchResultsAsShuffleSource"] = value;
+            }
+        }
     }
 }
